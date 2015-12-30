76efd175df88e32bf2ed7d04ab9d0669bd0e63e3
Gurpreet Atwal <gatwal@Live.com>
2015-12-30 05:21:46 -0800

    Create basic user model that extends an Eloquent model

--- src/models/User.php
+++ src/models/User.php
@@ -1,7 +1,27 @@
 <?php
 namespace SkeletonAPI\Models;
 
-class User
-{
+use Illuminate\Database\Eloquent\Model;
 
+class User extends Model
+{
+    /**
+     * @var string  Name of the table to use within the database
+     */
+    protected $table = 'users';
+
+    /**
+     * @var string  Primary key of the model within the table
+     */
+    protected $key = 'uid';
+
+    /**
+     * @var array   Attributes that are hidden from the model's array and JSON forms
+     */
+    protected $hidden = ['password'];
+
+    /**
+     * @var array   Attributes that cannot be assigned via mass-assignment
+     */
+    protected $guarded = ['password', 'updated_at', 'created_at'];
 }
