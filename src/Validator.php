db50318bdcf66c07c261343f29cc3aa8b9c9ac38
Gurpreet Atwal <gatwal@Live.com>
2016-01-02 22:50:14 -0800

    Refactor and write documentation for Validator

--- src/Validator.php
+++ src/Validator.php
@@ -6,17 +6,37 @@ use Respect\Validation\Rules\AllOf;
 use Respect\Validation\Rules\Attribute;
 
 /**
- * Allow for using Siriusphp/validation-like syntax to create rules for the respect validation package
- * Still needs a good amount of work, this is more of a prototype than anything
+ * Extension of Respect\Validation that allows for the the validation rules to be specified by strings
  *
- * Rule Syntax
- *  "field" => "StringType|Length(1,15)|Email|"
+ * This class allows for an creation of a validator by specifying the rules as strings rather than chained methods on an
+ * instance of the validator. This is fairly useful when trying to validate a model as the rules can be stored in a
+ * property of the model and then used to create a validator whenever one is needed. The basic format of a rule is a
+ * key, value pair where the key is the name of the field that needs to be validated and the value is a list of
+ * validation rules separated by the pipe | character. If you wish to specify a name you can provide it as a part of the
+ * key by inserting a colon followed by the name, do not use SetName. Most of rules provided in the Respect\Validation
+ * package, other than the 'Group Validators' should work.
  *
- * Class Validator
- * @package SkeletonAPI
+ * Example rules array:
+ * $rules = [
+ *      'email' => 'StringType|Email',
+ *      'password' => 'StringType|Length(8,null)',
+ *      'question:Security Question' => 'StringType|Length(8,null)',
+ *      'answer:Security Answer' => 'StringType|Length(8,null)',
+ * ]
+ * @package     SkeletonAPI
+ * @author      Gurpreet Atwal
+ * @license     MIT
+ * @see         https://github.com/Respect/Validation/blob/master/docs/VALIDATORS.md List of validation rules that can be used
+ * @see         https://github.com/Respect/Validation/blob/master/docs/README.md     Usage
+ * @todo        Write tests
  */
 class Validator extends \Respect\Validation\Validator
 {
+    /**
+     * Validator constructor.
+     * @param array $rules Array where each key corresponds to one attribute of the model and the value is a list of
+     *                     validation rules supported by Respect\Validation separated by the pipe ("|") character.
+     */
     public function __construct(array $rules)
     {
         foreach ($rules as $field => $rule) {
@@ -26,13 +46,15 @@ class Validator extends \Respect\Validation\Validator
     }
 
     /**
-     * @param $field
-     * @param $rule
+     * Adds a single attribute to this validator.
+     * @param string $field The attribute these rules apply to. Optionally specify a name for the rules by adding a
+     *                      colon(":") followed by the name. E.g. "question:Security Question"
+     * @param string $rule List of validators from the Respect\Validation package separated by the pipe ("|") character.
      */
     public function addAttribute($field, $rule)
     {
         $attributeRule = new AllOf();
-        $attributeRule->addRules($this->createRuleArray($rule));
+        $attributeRule->addRules($this->createRulesArray($rule));
         $name = $field;
 
         // Get name if one was provided
@@ -46,16 +68,26 @@ class Validator extends \Respect\Validation\Validator
         $this->addRule($attribute);
     }
 
-    private function createRuleArray($rules)
+    /**
+     * Takes a list of validation rules and returns them as an array of instantiated classes.
+     * @param  string $_rules List of validators from the Respect\Validation package separated by the pipe ("|") character.
+     * @return array         Array of validation rules
+     */
+    private function createRulesArray($_rules)
     {
-        $validators = [];
-        $rules = explode("|", $rules);
-        foreach ($rules as $rule) {
-            array_push($validators, $this->createRule($rule));
+        $rules = [];
+        $_rules = explode("|", $_rules);
+        foreach ($_rules as $rule) {
+            array_push($rules, $this->createRule($rule));
         }
-        return $validators;
+        return $rules;
     }
 
+    /**
+     * Takes a string that represents a validation rule and its arguments and returns it as a instantiated class.
+     * @param  string $rule Name of the rule to create and its arguments in parenthesis. E.g. Length(8, null)
+     * @return \Respect\Validation\Rules\AbstractRule
+     */
     private function createRule($rule)
     {
         $args = [];
@@ -73,18 +105,24 @@ class Validator extends \Respect\Validation\Validator
         return new $rule(...$args);
     }
 
-    private function formatArgs($args)
+    /**
+     * Takes an array containing strings, trims ALL the values and converts numeric values to integers and 'null' to an
+     * actual null value.
+     * @param  array $_args An array of strings that need to be formatted
+     * @return array Formatted arguments, contains strings, integers and null
+     */
+    private function formatArgs(array $_args)
     {
-        $new_args = [];
-        foreach ($args as $arg) {
+        $args = [];
+        foreach ($_args as $arg) {
             $arg = trim($arg);
             if (strtolower($arg) === 'null') {
                 $arg = null;
             } else if (is_numeric($arg)) {
                 $arg = intval($arg);
             }
-            array_push($new_args, $arg);
+            array_push($args, $arg);
         }
-        return $new_args;
+        return $args;
     }
 }
