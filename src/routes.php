0ec255a450581115aa5aadd90b5e2867ec78b615
Gurpreet Atwal <gatwal@Live.com>
2016-01-03 00:17:21 -0800

    Change router parameter format from Slim2 to Slim3

--- src/routes.php
+++ src/routes.php
@@ -3,9 +3,9 @@
 $app->group('/user', function () {
     $this->get('', '\SkeletonAPI\Controllers\User:all');
     $this->post('', '\SkeletonAPI\Controllers\User:create');
-    $this->get('/:uid', '\SkeletonAPI\Controllers\User:find');
-    $this->put('/:uid', '\SkeletonAPI\Controllers\User:update');
-    $this->delete('/:uid', '\SkeletonAPI\Controllers\User:delete');
+    $this->get('/{uid}', '\SkeletonAPI\Controllers\User:find');
+    $this->put('/{uid}', '\SkeletonAPI\Controllers\User:update');
+    $this->delete('/{uid}', '\SkeletonAPI\Controllers\User:delete');
 });
 $app->post('/login', '\SkeletonAPI\Controllers\Auth:login');
 $app->post('/recover', '\SkeletonAPI\Controllers\Auth:recover');
