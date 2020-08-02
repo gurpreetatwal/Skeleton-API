ce2d7a776821d0a6c04071fc2d598fa7efdaefee
Gurpreet Atwal <git@gatwal.com>
2020-08-01 22:38:29 -0700

    first draft

---
+++ src/Controllers/Auth.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace SkeletonAPI\Controllers;
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+/**
+ * Class Auth
+ * @package SkeletonAPI\Controllers
+ *
+ * Authentication controller handles all aspects of authentication including registration, login, and password-reset
+ */
+class Auth extends AbstractController
+{
+    public function login(Request $request, Response $response, array $args) {
+        
+    }
+
+    public function recover(Request $request, Response $response, array $args) {
+
+    }
+
+    public function reset(Request $request, Response $response, array $args) {
+
+    }
+}
